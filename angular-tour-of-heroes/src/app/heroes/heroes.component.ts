1d5807ffefc46e6c83d536c788967ba946ec94b4
Kevin Magno <kmagno94@gmail.com>
2018-09-13 17:36:35 -0400

    Part 6 in progress

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +10,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   // Add a heroes property to the class that exposes these heroes for binding.
-  heroes = HEROES;
+  heroes: Hero[];
 
   // Add the click event handler
   // Rename the component's hero property to selectedHero but don't assign it. There is no selected hero when the application starts.
   // Add the following onSelect() method, which assigns the clicked hero from the template to the component's selectedHero.
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
