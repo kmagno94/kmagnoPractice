38edf3240bd90eb71303fc2c14f1ac0a53c33bcc
Kevin Magno <kmagno94@gmail.com>
2018-10-17 13:56:04 -0400

    Completed WS using Jersey

---
+++ JAX-RS-Starter/src/com/revature/MyRestApp.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/app")
+public class MyRestApp extends Application {
+
+	
+	
+}
